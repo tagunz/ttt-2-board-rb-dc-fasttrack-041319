b7468e021aab79615caf3164a39220c7334e2968
Tomas Gunz <tagunz@hotmail.com>
2019-04-13 16:29:46 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -11,14 +11,14 @@
 row_one[0]
  
 # Position 2: Middle
-row[1]
+row_one[1]
  
 # Position 3: Right
-row[2]
+row_one[2]
  
 # Move X to the Position 2, Middle
-row[1] = "X"
-row #=> [" ", "X", " "]
+row_one[1] = "X"
+row_one => [" ", "X", " "]
 
 # Move X to the Position 1, Left
 row[0] = "X"

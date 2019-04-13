72b549fe298cddc76a99eb461737a9dcd41dc196
Tomas Gunz <tagunz@hotmail.com>
2019-04-13 16:36:36 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -5,7 +5,7 @@
 -----------   
  7 | 8 | 9 
  
- row_one = [" ", " ", " "]
+row_one = [" ", " ", " "]
  
 # Position 1: Left
 row_one[0]
@@ -16,14 +16,16 @@ row_one[1]
 # Position 3: Right
 row_one[2]
  
-# Move X to the Position 2, Middle
-row_one[1] = "X"
-# row_one => [" ", "X", " "]
-
 # Move X to the Position 1, Left
 row[0] = "X"
 # row_one => ["X", " ", " "]
 
+# Move X to the Position 2, Middle
+row_one[1] = "X"
+# row_one => [" ", "X", " "]
+
 # Move X to the Position 3, Right
 row[2] = "X"
 # row_one => [" ", " ", "X"]
+
+row_two = [" ", " ", " "]

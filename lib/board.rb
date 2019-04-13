773244f50fce82d88fe77ed0bf3115657d4d75d6
Tomas Gunz <tagunz@hotmail.com>
2019-04-13 17:27:55 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -5,66 +5,66 @@
 # -----------   
 # 7 | 8 | 9 
  
+# # //
+ 
+# row_one = [" ", " ", " "]
+ 
+# # Position 1: Left
+# row_one[0]
+ 
+# # Position 2: Middle
+# row_one[1]
+ 
+# # Position 3: Right
+# row_one[2]
+ 
+# # Move X to the Position 1, Left
+# row_one[0] = "X"
+# # row_one => ["X", " ", " "]
+
+# # Move X to the Position 2, Middle
+# row_one[1] = "X"
+# # row_one => [" ", "X", " "]
+
+# # Move X to the Position 3, Right
+# row_one[2] = "X"
+# # row_one => [" ", " ", "X"]
+
+# row_two = [" ", " ", " "]
+
+# # Position 4: Left
+# row_two[0]
+ 
+# # Position 5: Middle
+# row_two[1]
+ 
+# # Position 6: Right
+# row_two[2]
+ 
+# # Move X to the Position 4, Left
+# row_two[0] = "X"
+# # row_two => ["X", " ", " "]
+
+# # Move X to the Position 5, Middle
+# row_two[1] = "X"
+# # row_two => [" ", "X", " "]
+
+# # Move X to the Position 6, Right
+# row_two[2] = "X"
+# # row_two => [" ", " ", "X"]
+
 # //
- 
-row_one = [" ", " ", " "]
- 
-# Position 1: Left
-row_one[0]
- 
-# Position 2: Middle
-row_one[1]
- 
-# Position 3: Right
-row_one[2]
- 
-# Move X to the Position 1, Left
-row_one[0] = "X"
-# row_one => ["X", " ", " "]
 
-# Move X to the Position 2, Middle
-row_one[1] = "X"
-# row_one => [" ", "X", " "]
+# row_three = [" ", " ", " "]
 
-# Move X to the Position 3, Right
-row_one[2] = "X"
-# row_one => [" ", " ", "X"]
-
-row_two = [" ", " ", " "]
-
-# Position 4: Left
-row_two[0]
+# # Position 7: Left
+# row_three[0]
  
-# Position 5: Middle
-row_two[1]
+# # Position 8: Middle
+# row_three[1]
  
-# Position 6: Right
-row_two[2]
- 
-# Move X to the Position 4, Left
-row_two[0] = "X"
-# row_two => ["X", " ", " "]
-
-# Move X to the Position 5, Middle
-row_two[1] = "X"
-# row_two => [" ", "X", " "]
-
-# Move X to the Position 6, Right
-row_two[2] = "X"
-# row_two => [" ", " ", "X"]
-
-//
-
-row_three = [" ", " ", " "]
-
-# Position 7: Left
-row_three[0]
- 
-# Position 8: Middle
-row_three[1]
- 
-# Position 9: Right
-row_three[2]
+# # Position 9: Right
+# row_three[2]
  
 # Move X to the Position 7, Left
 row_three[0] = "X"

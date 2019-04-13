87e251419918654af331973dda9e85983b96468a
Tomas Gunz <tagunz@hotmail.com>
2019-04-13 16:33:42 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -18,6 +18,11 @@ row_one[2]
  
 # Move X to the Position 2, Middle
 row_one[1] = "X"
+row_one #=> [" ", "X", " "]
 
 # Move X to the Position 1, Left
 row[0] = "X"
+row_one #=> ["X", " ", " "]
+
+# Move X to the Position 3, Right
+
